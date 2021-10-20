b320dd1ffc0dafe50a9524bfc8e3f7a84bc9ed01
Sai Praneth Raju K <sai.praneth10@gmail.com>
2021-10-20 11:13:11 -0700

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,44 @@
+import socket, threading
+
+FADDR = input("Enter your friend's addr: ")
+PORT = 4444
+IP = "192.168.0.9"
+ADDR = (IP, PORT)
+
+cl_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #start the tcp connection
+cl_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #start the udp connection
+
+cl_tcp.connect(ADDR) #connect to the server
+cl_udp.bind(("", PORT+2)) #start the udp as host
+
+cl_tcp.send(FADDR.encode("utf-8"))
+print("[*] Waiting for your friend to join!")
+#waits for the servers response and ack-syn
+ack = cl_udp.recv(1024).decode("utf-8")
+if ack == "!Success":
+	print("[+] Success! Your friend is online")
+
+	def send_msgs():
+		connected = True
+		while connected:
+			msg = input("Msg: ")
+			if msg.lower() == "exit" or msg.lower() == "quit":
+				connected = False
+			cl_tcp.send(msg.encode("utf-8"))
+		print("[!] You disconnected from the server")
+
+	def recv_msgs():
+		connected = True
+		while connected:
+			inc_msg = cl_udp.recv(1024).decode("utf-8")
+			if inc_msg.lower() == "exit" or inc_msg.lower() == "quit":
+				connected = False
+			print(f"\n[{FADDR}]: {inc_msg}")
+		print("[!] Your friend has left the chat!")
+
+	recv_msgs_thread = threading.Thread(target=recv_msgs)
+	recv_msgs_thread.start()
+	send_msgs() #definition or function
+
+else:
+	print("[!] Your friend failed to connect")
---
+++ Server.py
@@ -0,0 +1,53 @@
+import socket, threading
+
+FADDR = {}
+PORT = 4444
+IP = "192.168.0.9"
+ADDR = (IP, PORT)
+
+sr_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #start the tcp connection
+sr_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #start the udp connection
+
+sr_tcp.bind(ADDR) #connect to the server
+sr_udp.bind(("", PORT+1)) #start the udp as host
+
+print("[+] Server is starting")
+def start_accept_connections():
+	print(f"[*] Listeneing on {ADDR[0]}:")
+	sr_tcp.listen()
+	while True:
+		fr_value = 0
+		conn, addr = sr_tcp.accept()
+		for i in FADDR:
+			if addr[0] == FADDR[i]:
+				fr_value = i
+				ack_msg = "!Success".encode("utf-8")
+				sr_udp.sendto(ack_msg, (fr_value, PORT+2))
+				ignore = conn.recv(1024)
+				sr_udp.sendto(ack_msg, (addr[0], PORT+2))
+			else:
+				fr_value = 0
+
+		thread = threading.Thread(target=handle_clients, args=(conn, addr, fr_value))
+		thread.start()
+		print(f"[*] Active connections: {threading.activeCount() - 1}")
+
+def handle_clients(conn, addr, fr_value):
+	global FADDR
+	print(f"[+] {addr[0]} connected on port {addr[1]}")
+	if fr_value != 0:
+		del FADDR[fr_value]
+	else:
+		msg_fddr = conn.recv(1024).decode("utf-8")
+		FADDR[addr[0]] = msg_fddr
+		fr_value = msg_fddr
+
+	while True:
+		msg = conn.recv(1024).decode("utf-8")
+		sr_udp.sendto(msg.encode("utf-8"), (fr_value, PORT+2))
+		if msg.lower() == "exit" or msg.lower() == "quit":
+			break
+	print(f"[+] {addr[0]} disconnected")
+	conn.close()
+
+start_accept_connections()
